e340217254a6d92a2ee40056c0ae2c17f78a4d75
fqdeng <fqdeng@trip.com>
2022-12-04 19:31:55 +0800

    Initial Commit

---
+++ NetworkDetecter/NetworkDetecterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkDetecterApp.swift
+//  NetworkDetecter
+//
+//  Created by winters on 2022/12/4.
+//
+
+import SwiftUI
+
+@main
+struct NetworkDetecterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
